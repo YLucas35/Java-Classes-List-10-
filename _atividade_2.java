fa60719a984171550b0fcf71849993595e6a3f15
YLucas35 <0001244748@senaimgaluno.com.br>
2026-05-06 11:02:25 -0300

    Create _atividade_2.java

---
+++ _atividade_2.java
@@ -0,0 +1,19 @@
+package Java_Lista_10_Array_2;
+
+import java.util.Scanner;
+
+public class _atividade_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] vect = new int[10];
+        int sum = 0;
+
+        System.out.println("Digite 15 números:");
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+
+        }
+    }
+}
